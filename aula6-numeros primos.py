6a94eb2deef950eef0af98559f089c3c01b8a5a9
tilirasampaio <tilirasampaio@gmail.com>
2026-04-16 14:58:50 -0300

    16/04

---
+++ aula6-numeros primos.py
@@ -0,0 +1,14 @@
+i = 0
+
+while i<=100:
+    j = i
+    controle = 0
+    while j > 0:
+        if i%j == 0:
+            controle += 1
+        if controle == 2:
+            print (i)
+        j -= 1
+
+
+i += 1
